68b901165dad5d35b564dd6232c335a1016f7339
Dolly Desir <dolly.desir@gmail.com>
2020-05-04 01:19:10 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -17,7 +17,6 @@ def update_status
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-         epic_tragedy[:capulet][:heroine][:status]= 'dead'
       }
    }
 
